46818a61178d9eb7c6595c21bff4dc50cc3e67e5
Dennelle <dennelle.walton@gmail.com>
2023-09-27 15:31:20 -0400

    learning how functions, parameters, and arguments work

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 console.log("this thing works!");
+
+//a function is either being defined or declared - creating the usable block of code to be used at a later time in the program.
+function sayHello(){
+// write the code to say hello here. you need to put the variable inside the curly brackets
+console.log(`Hello ${sayHello}`)
+}
+
+//execute the function, invoke the function, or call the function
+
+//write the functions name()
+//sayHello(personsName);
+
+//think of a function or a block of code like a set of instructions.
+//parameter
+
+function sayGoodbye(phrase,personsName) {
+console.log(`${phrase} , ${personsName}`)
+}
+
+sayGoodbye('adios', 'Laura');
+sayGoodbye('chao', 'Becky');
+sayGoodbye('adios', 'Chad');
+sayGoodbye('Peace', 'Frankie');
