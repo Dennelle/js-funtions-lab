ef76f1541e0c4ed2ed7b432895a4e427c08fcdcf
Dennelle <dennelle.walton@gmail.com>
2023-09-27 17:11:41 -0400

    completed question 2

--- js/app.js
+++ js/app.js
@@ -1,42 +1,99 @@
-console.log("this thing works!");
+// console.log("this thing works!");
 
-//a function is either being defined or declared - creating the usable block of code to be used at a later time in the program.
-function sayHello(){
-// write the code to say hello here. you need to put the variable inside the curly brackets
-console.log(`Hello ${sayHello}`)
+// //a function is either being defined or declared - creating the usable block of code to be used at a later time in the program.
+// function sayHello(){
+// // write the code to say hello here. you need to put the variable inside the curly brackets
+// console.log(`Hello ${sayHello}`)
+// }
+
+// //execute the function, invoke the function, or call the function
+
+// //write the functions name()
+// //sayHello(personsName);
+
+// //think of a function or a block of code like a set of instructions.
+// //parameter
+
+// function sayGoodbye(phrase,personsName) {
+// console.log(`${phrase} , ${personsName}`)
+// }
+
+// sayGoodbye('adios', 'Laura');
+// sayGoodbye('chao', 'Becky');
+// sayGoodbye('adios', 'Chad');
+// sayGoodbye('Peace', 'Frankie');
+
+
+// // return produces a value for the function when it is called. But, console.log only prints.
+
+// function getPlayerUsername(username){
+//     return username;
+// }
+
+
+// // before the game we don't know the players name
+// let playerOne;
+// let playerTwo;
+// let playerThree;
+// let playerFour;
+
+// // after they join the game we want to set the players name
+
+// console.log getPlayerUsername(dennelle)
+
+/* Requirements
+Define and code the functions below.
+Define the functions using the approach as specified (either as a function expression or declaration).
+Be sure to number each function with a comment above it.
+After each function, call it at least once and console.log the results.
+
+ 1.
+function maxOfTwoNumbers(x, y) {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+
+    // or more "elegantly" using the fantastic ternary expression!
+    // return  x >= y ? x : y;
+  }
+
+  console.log(maxOfTwoNumbers(3, 9));*/
+
+  //1.
+  function maxOfTwoNumbers(x,y){
+    if (x >= y) {
+        return x;
+      } else {
+        return y;
+      }
+
+  }
+
+  console.log(maxOfTwoNumbers(10,7))
+
+//2.
+
+function maxofThree(x,y,z) {
+    if (x > z&&y) {
+        return x;
+    } else if (y > x&&z) {
+        return y;
+} else if (z > x&&y) {
+        return z;
+    }
 }
+console.log(maxofThree(15,16,11));
 
-//execute the function, invoke the function, or call the function
-
-//write the functions name()
-//sayHello(personsName);
-
-//think of a function or a block of code like a set of instructions.
-//parameter
-
-function sayGoodbye(phrase,personsName) {
-console.log(`${phrase} , ${personsName}`)
-}
-
-sayGoodbye('adios', 'Laura');
-sayGoodbye('chao', 'Becky');
-sayGoodbye('adios', 'Chad');
-sayGoodbye('Peace', 'Frankie');
+//3.
 
 
-// return produces a value for the function when it is called. But, console.log only prints.
 
-function getPlayerUsername(username){
-    return username;
-}
+//4.
 
 
-// before the game we don't know the players name
-let playerOne;
-let playerTwo;
-let playerThree;
-let playerFour;
 
-// after they join the game we want to set the players name
 
-console.log getPlayerUsername(dennelle)
+
+//5.
