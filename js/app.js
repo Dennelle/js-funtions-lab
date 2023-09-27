7ebe68053bb04b0637536142f8f7b2ba3669e369
Dennelle <dennelle.walton@gmail.com>
2023-09-27 15:55:48 -0400

    understanding console.log and return statement

--- js/app.js
+++ js/app.js
@@ -22,3 +22,21 @@ sayGoodbye('adios', 'Laura');
 sayGoodbye('chao', 'Becky');
 sayGoodbye('adios', 'Chad');
 sayGoodbye('Peace', 'Frankie');
+
+
+// return produces a value for the function when it is called. But, console.log only prints.
+
+function getPlayerUsername(username){
+    return username;
+}
+
+
+// before the game we don't know the players name
+let playerOne;
+let playerTwo;
+let playerThree;
+let playerFour;
+
+// after they join the game we want to set the players name
+
+console.log getPlayerUsername(dennelle)
