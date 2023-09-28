ca837b104f6ef8063e1c60f00689857cc9a19848
Dennelle <dennelle.walton@gmail.com>
2023-09-28 04:58:19 -0400

    updated question 2 to be written as an expression

--- js/app.js
+++ js/app.js
@@ -72,8 +72,22 @@ function maxOfTwoNumbers(x, y) {
   }
   console.log(maxOfTwoNumbers(10,7))
 
+//2. Define a function, as a function expression, maxOfThree that takes three numbers as arguments and returns the largest of them. Again, the Math.max method is not allowed.
+
+// function maxOfThree(x,y,z) {
+//     if (x > z && x > y) {
+//         return x;
+//     } else if (y > x && y > z) {
+//         return y;
+// } else if (z > x && z > y) {
+//         return z;
+//     }
+// }
+// console.log(maxOfThree(14,17,16));
+
+
 //2.
-function maxOfThree(x,y,z) {
+let maxOfThree = function(x,y,z) {
     if (x > z && x > y) {
         return x;
     } else if (y > x && y > z) {
@@ -83,46 +97,3 @@ function maxOfThree(x,y,z) {
     }
 }
 console.log(maxOfThree(14,17,16));
-
-//3.
-function isCharAVowel(letter){
-    if (letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u') return true;
-};
-console.log(isCharAVowel('t'));
-
-//4.
-const sumArray = [1,2,3,4];
-let sum =0;
-for (i=0; i < sumArray.length; i++) {
-    sum += sumArray[i]
-};
-console.log(sum);
-
-//5.
-const multiplyArray = [1,2,3,4];
-let sum2 =0;
-for (i=0; i < sumArray.length; i++) {
-    sum2 = (sum2 * sumArray[i])
-};
-console.log(sum);
-
-//6. Define a function, as a function expression, numArgs that returns the number of arguments passed to the function when called.
-
-function numArgs(...nums) {
-    return nums.length;
-};
-console.log(numArgs(7,8,12));
-console.log(numArgs(7,8,12, 30, 25, 1));
-
-//7.Define a function, as a function declaration, reverseString that takes a string, reverses the characters, and returns it. For example, reverseString('rockstar'); would return the string "ratskcor".
-
-function reverseString(){
-
-}
-
-console.log();
-
-//8. Define a function, as a function expression, longestStringInArray that takes an array of strings as an argument and returns the length of the longest string.
-
-
-//9. Define a function, as a function declaration, stringsLongerThan that takes an array of strings and a number as arguments; and returns an array of the strings that are longer than the number passed in. For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
